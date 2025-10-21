852a4c36df2b56ab9634f8524962002b90d65457
pqz-miao <166804039+pqz-miao@users.noreply.github.com>
2025-10-21 20:18:54 +0800

    add landing page (#50)
    Co-authored-by: miao-pqz <1125583676.qq.com>

--- app/app.vue
+++ app/app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello World
-    </h1>
-    <button class="btn btn-secondary">
-      View Map <Icon name="tabler:map" size="40" />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
