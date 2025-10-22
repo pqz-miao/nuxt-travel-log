98fe727272c59ea5b97aa003194da5e7c9cc6301
miao-pqz <1125583676.qq.com>
2025-10-22 08:57:35 +0800

    typesafe env setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
+import "./app/utils/env";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
