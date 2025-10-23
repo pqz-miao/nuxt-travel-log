89d7bb751bf0885f15bb692f8c356e44bf3f287f
pqz-miao <166804039+pqz-miao@users.noreply.github.com>
2025-10-23 15:18:10 +0800

    add login to github (#55)
    Co-authored-by: miao-pqz <1125583676.qq.com>

---
+++ app/components/auth-button.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+</script>
+
+<template>
+  <button
+    :disabled="authStore.loading"
+    class="btn btn-accent"
+    @click="authStore.signIn"
+  >
+    Sign In With Github
+    <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
+    <Icon
+      v-else
+      name="tabler:brand-github"
+      size="24"
+    />
+  </button>
+</template>
---
+++ app/error.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+const route = useRoute();
+const error = route.query.error || "An unknown error occured.";
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 max-h-72">
+    <div role="alert" class="alert alert-error">
+      <span>Oh no! {{ error }}</span>
+    </div>
+  </div>
+</template>
