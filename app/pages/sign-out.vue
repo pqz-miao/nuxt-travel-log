57a8eae931ea9c27bf6784aaaa508646a2f211cf
pqz-miao <166804039+pqz-miao@users.noreply.github.com>
2025-10-23 19:49:08 +0800

    add auth state and sign out (#56)
    Co-authored-by: miao-pqz <1125583676.qq.com>

---
+++ app/pages/sign-out.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.signOut();
+});
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col items-center justify-center gap-4">
+    <span class="loading loading-spinner loadoing-xl" />
+  </div>
+</template>
