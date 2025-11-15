0aea5d1b174c277016db141dea658e8858089621
miao-pqz <1125583676.qq.com>
2025-11-15 16:36:32 +0800

    add get locations with user id

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -5,14 +5,9 @@ import slugify from "slug";
 import { findLocationByName, findUniqueSlug, insertLocation } from "~/utils/db/queries/locations";
 import { insertLocationSchema } from "~/utils/db/schemas";
 
-export default defineEventHandler(async (event) => {
-  if (!event.context.user) {
-    return sendError(event, createError({
-      statusCode: 401,
-      statusMessage: "Unauthorized",
-    }));
-  }
+import defineAuthenticatedEventHandler from "../utils/define-authenticated-event-handler";
 
+export default defineAuthenticatedEventHandler(async (event) => {
   const result = await readValidatedBody(event, insertLocationSchema.safeParse);
 
   if (!result.success) {
